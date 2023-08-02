6dafb5c154fc3500a16d251702e088a1f209d9b9
laze03 <Lazrak.edu@gmail.com>
2023-08-02 21:37:02 +0100

    finalize socket

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -5,51 +5,13 @@ import axios from "../../api/axios.js";
 import { useTheme } from "@mui/material";
 import { useContext } from "react";
 import AuthContext from "../../context/AuthProvider";
-import { makeStyles } from "@mui/styles";
-import { createStyles } from "@mui/material";
 import NotificationForm from "../notificationForm/NotificationForm";
-import NotificationCard from "./components/NotificationCard";
+import NotificationScroll from "./components/NotificationScroll";
 
 function Homepage() {
   const theme = useTheme();
   tokens(theme.palette.mode);
   useContext(ColorModeContext);
-
-  const { socket } = useContext(AuthContext);
-
-  const [notifications, setNotification] = useState([]); // ["email1", "email2"]
-
-  // const getNotifications = async (req, res) => {
-  //   const response = await axios.get("/notifications", {
-  //     headers: {
-  //       Authorization: `Bearer ${localStorage.getItem("token")}`,
-  //     },
-  //   });
-  //   console.log(response.data);
-  // };
-
-  const getNotifications = async () => {
-    const response = await axios.post("/notifications", {
-      id: localStorage.getItem("id"), //to change to get with header authorization
-    });
-    console.log(response.data);
-    setNotification(response.data);
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await getNotifications();
-    };
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    // Event listener for "newNotificationCreated" event
-    socket.on("newNotificationCreated", () => {
-      console.log("New notification created");
-      getNotifications(); // Fetch the latest notifications
-    });
-  }, []);
 
   return (
     <Box position={"relative"}>
@@ -58,33 +20,13 @@ function Homepage() {
       </Box>
       <Box
         width={"30vw"}
-        backgroundColor={theme.palette.background.popper}
-        px={5}
-        py={3}
-        overflow="auto"
         position="absolute"
         right={0}
         top={0}
+        overflow="auto"
         height={"100%"}
       >
-        <NotificationCard
-          notification={{
-            title: "Notification title",
-            date: "2021-10-10",
-            sender: "Sender name",
-            description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet",
-          }}
-        />
-        {notifications &&
-        notifications.length > 0 &&
-        Array.isArray(notifications) ? (
-          notifications.map((notification, index) => (
-            <NotificationCard notification={notification} key={index} />
-          ))
-        ) : (
-          <Box>No notifications</Box>
-        )}
+        <NotificationScroll />
       </Box>
     </Box>
   );
